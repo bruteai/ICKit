cec2d6ab44e4a39b3d5080931e213203983a8643
bruteai <57473568+bruteai@users.noreply.github.com>
2021-09-25 22:52:51 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-# Model Training
+# Train the model
 from argparse import ArgumentParser
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras import datasets, layers, models, losses, Model
