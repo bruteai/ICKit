2d0e0f0fe18809bcd012636ae3f1c1bb8d3d3dea
Umer <umer@aithe.io>
2021-10-05 15:14:37 +0900

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,3 @@
-# Preprocessor to load data
 from argparse import ArgumentParser
 import os
 from keras.preprocessing.image import ImageDataGenerator
@@ -14,62 +13,9 @@ from tensorflow.image import grayscale_to_rgb,resize
 import tensorflow as tf
 from sklearn.utils import shuffle
 from tensorflow.keras.utils import to_categorical
-class CIFAR10old:
-    def __init__(self,samples,path='dataset/old'):
-        self.dataset_path=path
-        
-    def add_test_to_train(self):
-        train_path=os.path.join(self.dataset_path,'train')
-        test_path=os.path.join(self.dataset_path,'test')
-        class_name=os.listdir(test_path)
-        for name in class_name:
-            for root, dirs, files in os.walk(os.path.join(test_path,name)):
-                train_folder=root.replace('test','train')
-                print(train_folder)
-                print('root',root)
-                print(len(files))
-                files_to_move=[os.path.join(root,f) for f in files[:-5]]
-                print(len(files_to_move))
-                #for name in files:
-                #    r.append(os.path.join(root, name))
-                #    return r
-                
-    def load_data(self):
-        print('Loading data....')
-        train_path=os.path.join(self.dataset_path,'train')
-        valid_path=os.path.join(self.dataset_path,'test')
-        train_datagen = ImageDataGenerator(rescale=1./255,
-                                           #shear_range=0,
-                                           #zoom_range=0,
-                                           #rotation_range=30,
-                                           vertical_flip=False,
-                                           width_shift_range=0.1,
-                                           height_shift_range=0.1)
-
-        test_datagen = ImageDataGenerator(rescale=1./255)  
-
-        train_generator = train_datagen.flow_from_directory(
-                train_path,
-                target_size=(32, 32),
-                batch_size=32,
-                class_mode='categorical',
-                shuffle=False)
-        
-        validation_generator = test_datagen.flow_from_directory(
-                valid_path,
-                target_size=(32, 32),
-                batch_size=32,
-                class_mode='categorical',
-
-                shuffle=False)
-        
-        return train_generator,validation_generator
-
          
-        
-       
 class CIFAR10:
-    def __init__(self,samples,path='dataset/CIFAR10'):
+    def __init__(self,path='dataset/CIFAR10'):
         self.dataset_path=path
         
     def preprocess(self):
@@ -134,33 +80,16 @@ class EMNIST():
         pass
     
     def preprocess_data(self,X_train, y_train, X_test, y_test):
-        #imgs_out = np.zeros((100,rows,cols,channels))
-        #X_train = np.expand_dims(X_train, axis=-1)
-        #X_test = np.expand_dims(X_test, axis=-1)
-        #print(X_train.shape)
         print('Preprocessing data....')
-        X_train=X_train[:80000,:,:]
-        y_train=y_train[:80000]
+        X_train=X_train[:50000,:,:]
+        y_train=y_train[:50000]
         n,rows,cols=X_train.shape
         X_train = grayscale_to_rgb(tf.expand_dims(X_train, axis=3),name=None)
-        #X_train = np.expand_dims(X_train, axis=-1).repeat(3, -1)
         X_test = grayscale_to_rgb(tf.expand_dims(X_test, axis=3),name=None)
-        print('done')
-        X_train_out = resize(X_train, [100,100])
-        X_test_out = resize(X_test, [100,100])
-        #X_train_out = np.zeros((X_train.shape[0],32,32,3))
-        #for n,i in enumerate(X_train):
-        #    X_train_out[n,:,:,:] = resize(X_train[n,:,:,:], X_train_out.shape[1:], anti_aliasing=True)
-            
-        #X_test_out = np.zeros((X_test.shape[0],32,32,3))
-        #for n,i in enumerate(X_test):
-        #    X_test_out[n,:,:,:] = resize(X_test[n,:,:,:], X_test_out.shape[1:], anti_aliasing=True)
-        #X_train = X_train.reshape(X_train.shape[0], rows, cols, )
-        #X_test = X_test.reshape(X_test.shape[0], rows, cols, 1)
-        X_train_out /= 255
-        X_test_out /= 255
-        #X_train_out, y_train = shuffle(X_train_out, y_train)
-        #X_test_out, y_test = shuffle(X_test_out, y_test)
+        X_train_out = resize(X_train, [50,50])
+        X_test_out = resize(X_test, [50,50])
+        X_train_out=np.divide(X_train_out,255.0)
+        X_test_out=np.divide(X_test_out,255.0)
         y_train = to_categorical(y_train, 26)
         y_test = to_categorical(y_test, 26)
         print('Preprocessing Finished!!!\n')
@@ -176,13 +105,6 @@ class EMNIST():
         print('Data Successfully Loaded!!!\n')
         return X_train, y_train, X_test, y_test
         
-    def visualise(self,X_train,y_train):
-        fig, ax = plt.subplots(5, 5, figsize=(10,10))
-        for i, axi in enumerate(ax.flat):
-            axi.imshow(X_train[i])
-            axi.set(xticks=[], yticks=[],xlabel=chr(y_train[i]+64))
-        
-        plt.show()
                 
     
 if __name__ == '__main__':
